1edc62e46a429a05e465c1706c734fab50a70286
Nidhi Elango <nidhielango3@gmail.com>
2022-05-19 10:46:51 -0400

    projects page

--- src/Project/Project.js
+++ src/Project/Project.js
@@ -19,10 +19,46 @@ function Project() {
               </div>
               <a href="/">
                 <div className='project-header'>
-                  <p>Title</p>
+                  <h1>Title</h1>
+                  <p>Subtitle</p>
                 </div>
                 <div className='project-image image1'>
-                  
+                </div>
+                <div className='project-footer'>
+                  <p>Description...</p>
+                  <img src={link} alt="" />
+                </div>
+              </a>
+            </div>
+
+            <div className="item">
+              <div className='project-number'>
+                <p>1</p>
+              </div>
+              <a href="/">
+                <div className='project-header'>
+                  <h1>Title</h1>
+                  <p>Subtitle</p>
+                </div>
+                <div className='project-image image1'>
+                </div>
+                <div className='project-footer'>
+                  <p>Description...</p>
+                  <img src={link} alt="" />
+                </div>
+              </a>
+            </div>
+
+            <div className="item">
+              <div className='project-number'>
+                <p>1</p>
+              </div>
+              <a href="/">
+                <div className='project-header'>
+                  <h1>Title</h1>
+                  <p>Subtitle</p>
+                </div>
+                <div className='project-image image1'>
                 </div>
                 <div className='project-footer'>
                   <p>Description...</p>
